5a66be7a281aeaca6d462496923b10260d1fd69f
Volodymyr Korniychuk <korniychuk49@gmail.com>
2023-03-16 18:31:58 +0200

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+//  1. створення і рендер розмітки на підставі масиву даних
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </div>
+     `;
+    })
+    .join('');
+}
+
+listEl.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+// код бібліотеки
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+  captionSelector: 'img',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
