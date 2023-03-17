8d71f0bb84583ba1bc58d03061070be104cb95f8
Volodymyr Korniychuk <korniychuk49@gmail.com>
2023-03-17 19:00:14 +0200

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,24 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const KEY = 'videoplayer-current-time';
+
+// код ліби
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+// відстежуємо подію 'timeupdate' - оновлення часу відтворення.
+const onPlay = function (data) {
+  const currentTime = data.seconds;
+  // Зберігаємо час відтворення у локальне сховище.
+  localStorage.setItem(KEY, currentTime);
+  console.log('currentTime', currentTime);
+};
+// Отримуємо збережений час
+const saveTime = localStorage.getItem(KEY);
+if (saveTime) {
+  // Відновлення відтворення зі збереженої позиції після перезавантаження сторінки.
+  player.setCurrentTime(saveTime);
+}
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.on('timeupdate', throttle(onPlay, 1000));
