c18fd609f0f6b1df62cf8a1d65dff46d9f577946
Volodymyr Korniychuk <korniychuk49@gmail.com>
2023-03-19 20:13:24 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+populateInput();
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+formEl.addEventListener('input', onFormTextInput);
+
+function onFormTextInput(event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateInput() {
+  const savedText = localStorage.getItem(STORAGE_KEY);
+  const parsed = JSON.parse(savedText);
+
+  if (parsed) {
+    inputEl.value = parsed.email || '';
+    textareaEl.value = parsed.message || '';
+  }
+}
